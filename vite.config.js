e840b0426ff28ce2b23284af2e91a694048d76b4
Quan <quannm25102004@gmail.com>
2024-10-19 20:24:31 +0700

    update file vite

--- vite.config.js
+++ vite.config.js
@@ -11,4 +11,12 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    build: {
+        outDir: 'dist', // Thư mục đầu ra mặc định là 'dist', bạn có thể thay đổi nếu cần.
+        rollupOptions: {
+          output: {
+            entryFileNames: 'bundle.js', // Tên file đầu ra
+          }
+        }
+      }
 });
